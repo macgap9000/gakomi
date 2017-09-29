cff7bc008b10791ffbdc58c2c5f6f36bf6e6b765
macgap9000 <macgap9000@gmail.com>
2017-09-29 17:37:17 +0200

    Poprawienie klasy generatora tokenów.

--- server/class/TokenGenerator.php
+++ server/class/TokenGenerator.php
@@ -15,27 +15,46 @@
         {
             // Podłączenie pliku klasy obiektu wyniku generowania tokena:
             require_once __DIR__ . '/TokenGenerationResult.php';
+            // Powołanie obiektu wyniku generowania tokena:
+            $objTokenGenerationResult = new TokenGenerationResult();
 
             // Próba wygenerowania tokena:
             try
             {
-                // Generowanie losowych wartości binarnych z użyciem biblioteki kryptograficznej:
+                // Generowanie losowych wartości binarnych 
+                // z użyciem biblioteki kryptograficznej:
                 $string = random_bytes(16);
+
+                // Konwersja wygenerowanych wartości binarnych do heksadecymalnych:
+                $token = bin2hex($string);
+
+                // Przykład wygenerowanego tokena:
+                // string(32) "89684b51ff2d6464e6c08b653d34e8ab"
+
+                // Generowanie tokena zakończone powodzeniem.
+                // Wypełnij obiekt wyniku generowania i go zwróć:
+                $objTokenGenerationResult->success = true;
+                $objTokenGenerationResult->errorCode = "ET999";
+                $objTokenGenerationResult->message = "Generowanie tokena zakończone pomyślnie.";
+                // Wpisanie do obiektu także wygenerowanego tokena:
+                $objTokenGenerationResult->token = $token;
+
+                // Zwrócenie wyniku generowania tokena:
+                return $objTokenGenerationResult;
             }
             catch (Exception $ex)
             {
-                // Przerwanie działania skryptu. Wystąpił błąd krytyczny:
-                die("Nie można wygenerować losowego tokena. Wystąpił nieznany błąd.");
+                // Przerwanie działania skryptu. Wystąpił błąd krytyczny.
+                // Nie można wygenerować tokena. Nieznany błąd.
+
+                // Wypełnij obiekt wyniku generowania i go zwróć:
+                $objTokenGenerationResult->success = false;
+                $objTokenGenerationResult->errorCode = "ET000";
+                $objTokenGenerationResult->message = "Problem z wygenerowaniem tokena.";
+
+                // Zwróć wynik generowania tokena:
+                return $objTokenGenerationResult;
             }
-            
-            // Konwersja wygenerowanych wartości binarnych do heksadecymalnych:
-            $token = bin2hex($string);
-
-            // Przykład:
-            // string(32) "89684b51ff2d6464e6c08b653d34e8ab"
-
-            // Zwrócenie wygenerowanego losowego tokena:
-            return $token;
         }
     }
 
